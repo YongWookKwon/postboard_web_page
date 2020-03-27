78aec0314abbee1cc668fec1c2de2f2077177129
taehyundev <K.anon.11107@gmail.com>
2020-03-27 16:51:17 +0900

    update 2020-03-27 (loginForm 배경 이미지 추가, css 전체적인 디자인, 로그인 했을 떄, 받은 세션에  insertId값으로 이름 출력 )

--- router/main/main.js
+++ router/main/main.js
@@ -1,12 +1,25 @@
 var express = require('express')
 var app = express()
 var router = express.Router();
+var mysql = require('mysql')
 var path  = require('path')
 
+var connection = mysql.createConnection({
+	host : 'localhost',
+	port : 3306,
+	user : 'root',
+	password : 'root',
+	database : 'postboardUser'
+})
+connection.connect()
+
 router.get('/', function(req,res){ // url /main으로 갔을 때
-    var id = req.user;
+	var id = req.user;
+	var name = ""
 	if(!id) res.render('login.ejs');
-	res.render('main.ejs', {'id' : id});
+	var query = connection.query(`select * from info where insertId='${id}'`, function(err,rows) {
+		res.render('main.ejs', {'id' : rows[0].name});
+	})
 });
 
 module.exports = router;
